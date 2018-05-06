281fdea43935eb00967879e03b7338e852e28380
nicoloso100 <nico.las0315@hotmail.com>
2018-05-06 00:28:04 -0500

    starting Android Build and PC Build (Final project directory, all the changes here must be optimized and functional, no test)

---
+++ PC Build/Assets/Scripts/HideMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideMenu : MonoBehaviour {
+
+    public GameObject VRConnection;
+    public GameObject EscenariosVRn;
+    
+    void Start () {
+        VRConnection.transform.position -= new Vector3(Screen.width, 0, 0);
+        EscenariosVRn.transform.position += new Vector3(Screen.width, 0, 0);
+    }
+}
---
+++ PC Build/Assets/Scripts/Hover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour {
+
+    public AudioSource HoverSound;
+
+    public void mouseEnter(GameObject WhatObject)
+    {
+        HoverSound.Play();
+        StartCoroutine(HoverEffect(WhatObject));
+    }
+
+    IEnumerator HoverEffect(GameObject WhatObject)
+    {
+        WhatObject.transform.localScale = new Vector3(-1, -1, 0);
+        yield return new WaitForSeconds(0.04F);
+        WhatObject.transform.localScale = new Vector3(1, 1, 1);
+    }
+}
