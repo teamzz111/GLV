e44764b62bb94a0de194aaf98a513af188867a8c
nicoloso100 <nico.las0315@hotmail.com>
2018-05-11 00:59:17 -0500

    started on play menu

---
+++ PC Build/Assets/Scripts/Menu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public GameObject menu;
+    public GameObject Camera;
+
+    private bool OpenMenu = false;
+
+    void Start () {
+        menu.SetActive(false);
+    }
+	
+	void Update () {
+        if (Input.GetKeyDown("escape"))
+        {
+            if(menu.transform.rotation.y == 0)
+            {
+                Camera.GetComponent<Animation>().Play("Menu");
+            }
+            else if(menu.transform.rotation.y == 180)
+            {
+                Camera.GetComponent<Animation>().Play("Resume");
+            }
+            OpenMenu = true;
+        }
+        if(OpenMenu == true)
+        {
+            if(menu.transform.rotation.y == 180)
+            {
+                menu.SetActive(true);
+                OpenMenu = false;
+            }
+            else if (menu.transform.rotation.y == 0)
+            {
+                menu.SetActive(false);
+                OpenMenu = false;
+            }
+        }
+    }
+}
